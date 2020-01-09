b4916eb9a5c4bdd2be80de69b6caf6d12c3d6e33
neeldug <dragon@dugar.co.uk>
2020-01-09 23:39:18 +0000

    Setting project folder up in new laptop!

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -8,6 +8,7 @@ export default class Hello extends Component {
         <h2>My Projects</h2>
         <div class ="container flex-container">
           <div class="portfolio-item">
+          <h3>Donde Esta la Bibliotheque?</h3>
           </div>
           <div class="portfolio-item">
           </div>
